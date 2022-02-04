86b48149d448d41fa23c6d0a95cd24b48dcaa719
dzimentey <dzimentey@gmail.com>
2022-02-04 19:44:15 +0300

    feat: add web interface

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,32 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 
 
 function App() {
-    return <div></div>
+
+    const [messages, setMessages] = useState([
+
+        {message: 'hello', id: '1234567', user: {id: '123456', name: 'Kolia'}},
+        {message: 'hello guys', id: '1234568', user: {id: '123457', name: 'Fedia'}},
+
+    ])
+
+    return (
+        <div className={'App'}>
+            <div style={{border: '1px solid black', padding: '10px',width: '300px', height: '300px', overflow: 'scroll'}}>
+                {messages.map(m => {
+                 return <div>
+                     <b>{m.user.name}:</b> {m.message}
+                     <hr/>
+                 </div>
+                })}
+            </div>
+            <div>
+                <textarea></textarea>
+                <button>Send</button>
+            </div>
+        </div>
+    )
 }
 
 export default App;
