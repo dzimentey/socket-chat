3a729ee181d832461c79d154efdad2637d576c1b
dzimentey <dzimentey@gmail.com>
2022-02-05 16:09:45 +0300

    feat: add socket connection and chat window

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,36 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import './App.css';
+import {io} from "socket.io-client";
 
+const socket = io('http://localhost:3009');
 
 function App() {
 
-    const [messages, setMessages] = useState([
+    useEffect( () => {
+        socket.on('got-init-messages', (initMessages) => {
+            setMessages(initMessages)
+        } )
+    }, [])
 
-        {message: 'hello', id: '1234567', user: {id: '123456', name: 'Kolia'}},
-        {message: 'hello guys', id: '1234568', user: {id: '123457', name: 'Fedia'}},
+    const [messages, setMessages] = useState([])
 
-    ])
+    const [message, setMessage] = useState("HI-hi")
 
     return (
         <div className={'App'}>
             <div style={{border: '1px solid black', padding: '10px',width: '300px', height: '300px', overflow: 'scroll'}}>
-                {messages.map(m => {
-                 return <div>
+                {messages.map((m: any) => {
+                 return <div key={m.id}>
                      <b>{m.user.name}:</b> {m.message}
                      <hr/>
                  </div>
                 })}
             </div>
             <div>
-                <textarea></textarea>
-                <button>Send</button>
+                <textarea value={message} onChange={(e) => setMessage(e.currentTarget.value)}></textarea>
+                <button onClick={()=>{socket.emit('message-sent', message);
+                                        setMessage('');
+                }}>Send</button>
             </div>
         </div>
     )
