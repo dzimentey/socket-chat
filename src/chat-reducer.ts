9c60b1e6a508f10595328709e3d681b74f376de3
dzimentey <dzimentey@gmail.com>
2022-02-11 17:55:46 +0300

    feat: add user is typing func

--- src/chat-reducer.ts
+++ src/chat-reducer.ts
@@ -1,18 +1,27 @@
 import {Dispatch} from "redux";
 import {api} from "./api";
 
-const initialState = {
-    messages: []
+const initialState: {messages: any[], typingUsers: any[]} = {
+    messages: [],
+    typingUsers: [],
 }
+
 type initialStateType = typeof initialState
 type ActionsChatType = ReturnType<typeof messagesReceivedAC> | ReturnType<typeof newMessageReceivedAC>
+                        | ReturnType<typeof typingUserAddedAC>
 
-export const chatReducer = (state: initialStateType = initialState, action: ActionsChatType) => {
+
+export const chatReducer = (state:initialStateType = initialState, action: ActionsChatType) => {
     switch (action.type) {
         case 'Messages-Received':
             return {...state, messages: action.messages}
         case 'New-Message-Received':
-            return {...state, messages: [...state.messages, action.message]}
+            return {...state,
+                messages: [...state.messages, action.message],
+                typingUsers: state.typingUsers.filter((u: any) => u.id !== action.message.user.id),
+            }
+        case "Typing-User-Added":
+            return {...state, typingUsers: [...state.typingUsers.filter((u: any) => u.id !== action.user.id), action.user]}
         default:
             return state
     }
@@ -20,16 +29,21 @@ export const chatReducer = (state: initialStateType = initialState, action: Acti
 
 export const messagesReceivedAC = (messages: any) => ({type: 'Messages-Received', messages}) as const
 export const newMessageReceivedAC = (message: any) => ({type: 'New-Message-Received', message}) as const
+export const typingUserAddedAC = (user: any) => ({type: 'Typing-User-Added', user}) as const
 
 export const createConnectionTC = () => (dispatch: Dispatch) => {
     api.createConnection()
     api.subscribe(
         (messages: any) => {
-        dispatch(messagesReceivedAC(messages))
-    },
+            dispatch(messagesReceivedAC(messages))
+        },
         (message: any) => {
-        dispatch(newMessageReceivedAC(message))
-    })
+            dispatch(newMessageReceivedAC(message))
+        },
+        (user: any) => {
+            dispatch(typingUserAddedAC(user))
+        },
+    )
 }
 
 export const setClientNameTC = (name: string) => (dispatch: Dispatch) => {
@@ -42,4 +56,8 @@ export const sendMessageTC = (text: string) => (dispatch: Dispatch) => {
 
 export const destroyConnectionTC = () => (dispatch: Dispatch) => {
     api.destroyConnection()
+}
+
+export const typingMessageTC = () => (dispatch: Dispatch) => {
+    api.typingMessage()
 }
